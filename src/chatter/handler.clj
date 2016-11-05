c8cd8faeaae9daebd8f86719a29b05584a9069ae
elisa lee <elisa.my.lee@gmail.com>
2016-11-05 14:23:50 -0500

    add hard-coded messages

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -4,17 +4,23 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
 
+(def chat-messages [{:name "blue" :message "hello, world"}
+                    {:name "red" :message "red is my favorite color"}
+                    {:name "green" :message "green makes it go faster"}])
 
 (defn generate-message-view
   "This generates the HTML for displaying messages"
-  []
+  [messages]
   (page/html5
    [:head
     [:title "chatter"]]
    [:body
-    [:h1 "Our Chat App"]]))
+    [:h1 "Our Chat App"]
+    [:p
+      [:table
+        (map(fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages )]]]))
 (defroutes app-routes
-  (GET "/" [] (generate-message-view))
+  (GET "/" [] (generate-message-view chat-messages))
   (route/not-found "Not Found"))
 
 (def app
